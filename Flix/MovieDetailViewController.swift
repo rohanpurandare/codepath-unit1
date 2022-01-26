27fc1c9dfc1ce357ac5779128ece3fdee38b67e3
Rohan Purandare <70453871+rohanpurandare@users.noreply.github.com>
2022-01-25 23:35:33 -0500

    Add files via upload

---
+++ Flix/MovieDetailViewController.swift
@@ -0,0 +1,54 @@
+ //
+//  MovieDetailViewController.swift
+//  Flix
+//
+//  Created by Rohan Purandare on 1/24/22.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieDetailViewController: UIViewController {
+    
+    @IBOutlet weak var backdropView: UIImageView!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    var movie: [String:Any]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit();
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let poster_path = movie["poster_path"] as! String
+        let posterURL = URL(string: baseUrl + poster_path)
+        
+        posterView.af.setImage(withURL: posterURL!)
+        
+        let backdrop_path = movie["backdrop_path"] as! String
+        let backdropURL = URL(string: "https://image.tmdb.org/t/p/w780" + backdrop_path)
+        
+        backdropView.af.setImage(withURL: backdropURL!)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Rohan Purandare on 1/25/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
